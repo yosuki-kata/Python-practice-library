da37451a06c7be9ec79dabb5991724cfaf989881
yosuki-kata <2492246897@qq.com>
2026-08-27 22:23:09 +0800

    Daily submission.

---
+++ Files/01.py
@@ -0,0 +1,4 @@
+f_name = ""
+l_name = ""
+final_name = f_name + " " + l_name
+print("Hello,",final_name.title(),"!")
---
+++ Files/02.py
@@ -0,0 +1,9 @@
+print("Languages:\n\tPython\nC++\nJavaScript")
+one = "python"
+print(one)
+one = one.upper()
+print(one)
+two = "Python "
+print(two)
+two = two.rstrip()
+print(two)
